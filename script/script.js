965ecab60315e0ded8dbc2ebc176e5c2d69130c3
Alex Crow <moon_storm@bk.ru>
2021-07-21 18:46:00 +0300

    ver 2.4

--- script/script.js
+++ script/script.js
@@ -1,13 +1,44 @@
-let buttonEdit = document.querySelector('.profile__button-edit');
-let buttonClose = document.querySelector('.popup__button-close');
-let popup = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let popupName = document.querySelector('.popup__field_name');
-let popupAbout = document.querySelector('.popup__field_about');
+//попап редактирования профиля 
+
+const buttonEdit = document.querySelector('.profile__button-edit');
+const buttonClose = document.querySelector('.popup__button-close');
+const popup = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const popupName = document.querySelector('.popup__field_name');
+const popupAbout = document.querySelector('.popup__field_about');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  popupName.value = profileName.textContent;
+  popupAbout.value = profileAbout.textContent;
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+buttonEdit.addEventListener('click', popupOpen);
+buttonClose.addEventListener('click', popupClose);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupAbout.value;
+  popupClose();
+}
+
+
+//попап добавления карточки
+
+const buttonAdd = document.querySelector('.profile__button-add');
+const buttonCloseAdd = document.querySelector('.popup__button-close_add');
+const popupAdd = document.querySelector('.popup__add');
 const formElement = document.querySelector('.popup__input');
-const initialCards = [
-  {
+const popupTitle = document.querySelector('popup__field_title');
+const popupSrc = document.querySelector('popup__field_src');
+const elementsList = document.querySelector('elements__list');
+const initialCards = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
@@ -33,24 +64,33 @@ const initialCards = [
   }
 ];
 
-function popupOpen() {
-    popup.classList.add('popup_opened');
-    popupName.value = profileName.textContent;
-    popupAbout.value = profileAbout.textContent;
+function popupAddOpen() {
+  popupAdd.classList.add('popup_opened');
 }
 
-function popupClose() {
-    popup.classList.remove('popup_opened');
+function popupAddClose() {
+  popupAdd.classList.remove('popup_opened');
 }
 
-buttonEdit.addEventListener('click', popupOpen);
-buttonClose.addEventListener('click', popupClose);
+buttonAdd.addEventListener('click', popupAddOpen);
+buttonCloseAdd.addEventListener('click', popupAddClose);
 
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = popupName.value;
-    profileAbout.textContent = popupAbout.value;
-    popupClose();
-}
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+function addElement(evt) {
+  evt.preventDefault();
+  const elementContainer = document.createElement('li');
+  elementContainer.classList.add('element');
+  const elementImage = document.createElement('img');
+  elementImage.classList.add('element__image');
+  elementImage.src = popupSrc.value;
+  const elementTitle = document.createElement('h2');
+  elementTitle.classList.add('element__title');
+  elementTitle.textContent = popupTitle.value;
+  elementContainer.prepend(elementImage, elementTitle);
+  elementsList.prepend(elementContainer);
+  popupAddClose();
+}
+
+formElement.addEventListener('submit', addElement);
