a5c539be16c6f5682506d1aadeb53d779f3257cd
Alex Crow <moon_storm@bk.ru>
2021-08-02 21:55:05 +0300

    ver 2.5

--- script/script.js
+++ script/script.js
@@ -1,96 +1,60 @@
-//попап редактирования профиля 
-
+//открытие и закрытие попапов
 const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonClose = document.querySelector('.popup__button-close');
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_edit');
+const buttonCloseEdit = document.querySelector('.popup__button-close_edit');
+const buttonCloseAdd = document.querySelector('.popup__button-close_add');
+const buttonSaveEdit = document.querySelector('.popup__button-save_edit');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-const popupName = document.querySelector('.popup__field_name');
-const popupAbout = document.querySelector('.popup__field_about');
+const popupEditName = document.querySelector('.popup__field_name');
+const popupEditAbout = document.querySelector('.popup__field_about');
+const formElement = document.querySelector('.popup__input');
+const buttonAdd = document.querySelector('.profile__button-add');
+const popupAdd = document.querySelector('.popup_add');
 
-function popupOpen() {
-  popup.classList.add('popup_opened');
-  popupName.value = profileName.textContent;
-  popupAbout.value = profileAbout.textContent;
+function popupOpen(popup) {
+    popup.classList.add('popup_opened');
 }
 
-function popupClose() {
-  popup.classList.remove('popup_opened');
+function popupClose(popup) {
+    popup.classList.remove('popup_opened');
 }
-
-buttonEdit.addEventListener('click', popupOpen);
-buttonClose.addEventListener('click', popupClose);
 
 function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profileName.textContent = popupName.value;
-  profileAbout.textContent = popupAbout.value;
-  popupClose();
+    evt.preventDefault();
+    profileName.textContent = popupEditName.value;
+    profileAbout.textContent = popupEditAbout.value;
+    popupClose(popupEdit);
 }
 
+buttonEdit.addEventListener('click', () => {
+    popupEditName.value = profileName.textContent;
+    popupEditAbout.value = profileAbout.textContent;
+    popupOpen(popupEdit);
+});
 
-//попап добавления карточки
+buttonAdd.addEventListener('click', () => {
+    popupOpen(popupAdd);
+});
 
-const buttonAdd = document.querySelector('.profile__button-add');
-const buttonCloseAdd = document.querySelector('.popup__button-close_add');
-const popupAdd = document.querySelector('.popup__add');
-const formElement = document.querySelector('.popup__input');
-const popupTitle = document.querySelector('popup__field_title');
-const popupSrc = document.querySelector('popup__field_src');
-const elementsList = document.querySelector('elements__list');
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+buttonCloseEdit.addEventListener('click', () => {
+    popupClose(popupEdit);
+});
 
-function popupAddOpen() {
-  popupAdd.classList.add('popup_opened');
-}
+popupEdit.addEventListener('click', (evt) => {
+    if (evt.target === evt.currentTarget) {
+        popupClose(popupEdit);
+    }
+});
 
-function popupAddClose() {
-  popupAdd.classList.remove('popup_opened');
-}
+popupAdd.addEventListener('click', (evt) => {
+    if (evt.target === evt.currentTarget) {
+        popupClose(popupAdd);
+    }
+});
 
-buttonAdd.addEventListener('click', popupAddOpen);
-buttonCloseAdd.addEventListener('click', popupAddClose);
-
+buttonCloseAdd.addEventListener('click', () => {
+    popupClose(popupAdd);
+});
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-function addElement(evt) {
-  evt.preventDefault();
-  const elementContainer = document.createElement('li');
-  elementContainer.classList.add('element');
-  const elementImage = document.createElement('img');
-  elementImage.classList.add('element__image');
-  elementImage.src = popupSrc.value;
-  const elementTitle = document.createElement('h2');
-  elementTitle.classList.add('element__title');
-  elementTitle.textContent = popupTitle.value;
-  elementContainer.prepend(elementImage, elementTitle);
-  elementsList.prepend(elementContainer);
-  popupAddClose();
-}
-
-formElement.addEventListener('submit', addElement);
